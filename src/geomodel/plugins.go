6439310c07f28b53b1e26f6cfb3c0e4569b5574f
Aaron Meihm <ameihm@mozilla.com>
2015-11-18 09:59:47 -0600

    add some validation to plugin output

--- src/geomodel/plugins.go
+++ src/geomodel/plugins.go
@@ -20,18 +20,46 @@ import (
 	"strings"
 )
 
+// Describes input sent to a plugin; primarily a slice of raw JSON documents
+// that have been returned by ES
 type pluginRequest struct {
-	Events []*json.RawMessage `json:"events"`
+	Events []*json.RawMessage `json:"events"` // Slice of matching plugin events
 }
 
+// Describes the result of execution of a plugin; plugins must return data
+// that conforms to this structure.
 type pluginResult struct {
-	Results []eventResult `json:"results"`
+	Results []eventResult `json:"results"` // Slice of event results
 }
 
+func (p *pluginResult) validate() error {
+	for i := range p.Results {
+		err := p.Results[i].validate()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Corresponds to an individual event in a plugin result
 type eventResult struct {
-	Principal  string `json:"principal"`
-	SourceIPV4 string `json:"source_ipv4"`
-	Valid      bool   `json:"valid"`
+	Principal  string `json:"principal"`   // Authentication principal, identifier
+	SourceIPV4 string `json:"source_ipv4"` // Source IPV4 for authentication
+	Valid      bool   `json:"valid"`       // True if entry was parsed correctly by plugin
+}
+
+func (e *eventResult) validate() error {
+	if !e.Valid {
+		return nil
+	}
+	if e.Principal == "" {
+		return fmt.Errorf("plugin result has no principal value")
+	}
+	if e.SourceIPV4 == "" {
+		return fmt.Errorf("plugin result has no source_ipv4 value")
+	}
+	return nil
 }
 
 type plugin struct {
@@ -53,6 +81,10 @@ func (p *plugin) runPlugin(input []byte) (err error) {
 	err = json.Unmarshal(output.Bytes(), &res)
 	if err != nil {
 		return fmt.Errorf("error parsing output from %v: %v", p.path, err)
+	}
+	err = res.validate()
+	if err != nil {
+		return fmt.Errorf("error validating plugin output from %v: %v", p.name, err)
 	}
 	pluginResultCh <- res
 	return nil
